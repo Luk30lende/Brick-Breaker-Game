2164c729b4ab35ae2fa739a3f82d7917be3bba71
Luke Olende <olendeluke2k@gmail.com>
2025-02-04 12:43:33 +0300

    Move paddle/player

--- breakout.js
+++ breakout.js
@@ -4,15 +4,17 @@ let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
-// Player
+// Player/Paddle
 let playerWidth = 80;
 let playerHeight = 10;
+playerVelocityX = 10;
 
 let player = {
   x: boardWidth / 2 - playerWidth / 2,
   y: boardHeight - playerHeight - 5,
   width: playerWidth,
   height: playerHeight,
+  velocityX: playerVelocityX,
 };
 
 window.onload = () => {
@@ -22,6 +24,38 @@ window.onload = () => {
   context = getContext("2d"); // Used for drawing on the board
 
   // Draw the player
-  context.fillStyle = "skyblue";
+  context.fillStyle = "lightgreen";
   context.fillRect(player.x, player.y, player.width, player.height);
+
+  requestAnimationFrame(update);
+  document.addEventListener("keydown", movePlayer);
+};
+
+const update = () => {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height); // clear the frame before we draw something new in the canvas
+
+  //Player/Paddle
+  context.fillStyle = "lightgreen";
+  context.fillRect(player.x, player.y, player.width, player.height);
+};
+
+function outOfBounds(xPosition) {
+  return xPosition < 0 || xPosition + playerWidth > boardWidth;
+}
+
+const movePlayer = (e) => {
+  if (e.code == "ArrowLeft") {
+    // player.x -= player.velocityX;
+    let nextPlayerX = player.x - player.velocityX;
+    if (!outOfBounds(nextPlayerX)) {
+      player.x = nextPlayerX;
+    }
+  } else if (e.code == "ArrowRight") {
+    // player.y += player.velocityX;
+    let nextPlayerX = player.x + player.velocityX;
+    if (!outOfBounds(nextPlayerX)) {
+      player.x = nextPlayerX;
+    }
+  }
 };
